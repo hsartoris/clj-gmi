ff31c28d86f7400cd0b7b5468cf9075ccf411e1f
Hayden Sartoris <hsartoris@gmail.com>
2020-09-05 16:59:38 -0400

    doesn't work

--- src/clj_gmi/server.clj
+++ src/clj_gmi/server.clj
@@ -1,57 +1,68 @@
 (ns clj-gmi.server
-  "The server part"
+  "The server part - basically weavejester/tcp-server"
   (:require [clojure.java.io :as io])
   (:import [javax.net.ssl
             SSLServerSocket
             SSLServerSocketFactory
             SNIHostName]
-           [java.net InetAddress]
+           [java.net InetAddress SocketException]
            [java.io Closeable]))
-
-(defonce connections (atom #{}))
-(defonce server (atom nil))
-
-(def ^:private default-config
-  {:port      1965
-   :backlog   0
-   :addr      "127.0.0.1"
-   :hostname  "localhost.localdomain"})
-
-(defrecord GeminiServer [port backlog addr hostname socket handler conns]
-  Closeable
-  (close [_]
-    (when-not (and (nil? socket) (.isClosed socket))
-      (.close socket))))
 
 (defn- create-server-socket 
   "Add an SSLServerSocket to the given config map"
-  [{:keys [port backlog addr] :as server}]
+  [{:keys [port backlog addr socket]}]
   (let [inet-addr (InetAddress/getByName addr)]
     (-> (SSLServerSocketFactory/getDefault)
         (.createServerSocket port backlog inet-addr)
-        (->> (assoc server :socket)))))
+        (->> (reset! socket)))))
 
 (defn- configure-sni
   "Configure an SSLServerSocket with SNI for a given hostname."
-  [{:keys [hostname socket] :as server}]
-  {:pre [socket]}
+  [{:keys [hostname socket]}]
+  {:pre [@socket]}
   (let [sni-hostname (SNIHostName. hostname)
-        ssl-params (-> (.getSSLParameters socket)
+        ssl-params (-> (.getSSLParameters @socket)
                        (.setServerNames [sni-hostname]))]
-    (.setSSLParameters socket ssl-params)
-    server))
+    (.setSSLParameters @socket ssl-params)))
 
-(defn configure
-  [config]
-  (-> (merge default-config config)
-      (assoc :conns (atom #{}))
-      (->> (reset! server))))
+(defn running? [{:keys [socket]}]
+  (when-some [socket @socket]
+    (not (.isClosed socket))))
 
-(defn start! 
-  ([] (swap! server create-server-socket))
-  ([config]
-   (configure config)
-   (start!)))
+(defn close-conn [{:keys [conns]} conn]
+  (swap! conns disj conn)
+  (when-not (.isClosed conn)
+    (.close conn)))
 
-(defn stop! []
-  (
+(defn- accept-conn
+  [{:keys [handler conns socket] :as server}]
+  (let [conn (.accept @socket)]
+    (swap! conns conj conn)
+    (future
+      (try (handler conn)
+           (finally (close-conn server conn))))))
+
+(defn gmi-server
+  [config handler]
+  (merge
+    {:port      1965
+     :backlog   0
+     :addr      "127.0.0.1"
+     :hostname  "localhost.localdomain"
+     :handler   handler
+     :conns     (atom #{})
+     :socket    (atom nil)}
+    config))
+
+(defn start! [server]
+  (create-server-socket server)
+  (configure-sni server)
+  (while (running? server)
+    (try
+      (accept-conn server)
+      (catch SocketException _))))
+
+(defn stop! [{:keys [socket conns] :as server}]
+  (doseq [conn @conns]
+    (close-conn server conn))
+  (.close @socket))
