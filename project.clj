f1340b1c646e57d1e73755f7c2f57741fb94883f
Hayden Sartoris <hsartoris@gmail.com>
2020-09-08 07:25:53 -0400

    deps, necessary and not; also SSL configuration

--- project.clj
+++ project.clj
@@ -3,8 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [lambdaisland/uri "1.4.54"]
+                 [metosin/reitit-core "0.5.5"]]
   :main ^:skip-aot clj-gmi.core
   :target-path "target/%s"
+  :jvm-opts ["-Djavax.net.ssl.keyStore=resources/keystore.jks"
+             "-Djavax.net.ssl.keyStorePassword=changeit"]
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
